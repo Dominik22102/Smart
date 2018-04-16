3ecc57872da0fb2f4416bb7a73a6bbb58a594ae0
Dominik22102 <37939144+Dominik22102@users.noreply.github.com>
2018-04-16 16:44:53 +0200

    Add files via upload

--- run.php
+++ run.php
@@ -8,6 +8,8 @@
 $url = "";
 if(isset($_GET['command'])) {
     $com = $_GET['command'];
-    shell_exec('/usr/local/bin/'.$com);
+	shell_exec('sudo pkill akt.sh');
+	shell_exec('sudo cp /usr/local/bin/'.$com.'/usr/local/bin/akt.sh');
+    shell_exec('/usr/local/bin/akt.sh');
     header("Location:index.php");
 }
